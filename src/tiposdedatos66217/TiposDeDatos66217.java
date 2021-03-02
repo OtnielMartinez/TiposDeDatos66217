746ab9bff619d2459a0e48404c33296933097dbf
lampi <lampi@192.168.1.78>
2021-03-02 07:29:30 -0600

    Commit para repocitorio

---
+++ src/tiposdedatos66217/TiposDeDatos66217.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatos66217;
+
+/**
+ *
+ * @author lampi
+ */
+public class TiposDeDatos66217 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valorminimoint= -2147483648;
+        int valormaximoint=2147483647;
+        long valorminimonlong = -9223372036854775808L;
+        long valormaximolong =  9223372036854775807L;
+        short valorminimonshort = -32768;
+        short valormaximoshort = 32767;
+        byte valorminimobyte = -128;
+        byte valormaximobyte = 127;
+        double valorminimodouble = -1.8e307;
+        double valormaximodouble = 1.8e307;
+        
+            System. out. println("Valor minimo de int = " + valorminimoint);
+            System. out. println("Valor maximo de int = " + valormaximoint);
+            System. out. println("Valor minimo de long = " + valorminimonlong);
+            System. out. println("Valor maximo de long = " + valormaximolong);
+            System. out. println("Valor minimo de short = " + valorminimonshort);
+            System. out. println("Valor maximo de short = " + valormaximoshort);
+            System. out. println("Valor minimo de byte = " + valorminimobyte);
+            System. out. println("Valor maximo de byte = " + valormaximobyte);
+            System. out. println("Valor minimo de double = " + valorminimodouble);
+            System. out. println("Valor maximo de double = " + valormaximodouble);
+            
+            
+    }
+    
+}
